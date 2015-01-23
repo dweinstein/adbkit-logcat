c7e6a62bd69c38b5cb9667ed158bc994bb748977
David Weinstein <dweinst@insitusec.com>
2015-01-23 17:09:28 -0500

    don't load coffee

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 (function() {
-  var Path;
-
-  Path = require('path');
-
   module.exports = (function() {
-    switch (Path.extname(__filename)) {
-      case '.coffee':
-        return require('./src/logcat');
-      default:
-        return require('./lib/logcat');
-    }
+    return require('./lib/logcat');
   })();
-
 }).call(this);
+
